9ff3d9d2a55419436333343993c692d027e8af09
John Ericson <John.Ericson@Obsidian.Systems>
2020-03-02 16:41:48 -0500

    Remove unneeded parens

--- release.nix
+++ release.nix
@@ -3,6 +3,6 @@ let
 in {
   analysis-nanos = ledger-app-nervos.clangAnalysis.s.wallet;
   release-nanos = ledger-app-nervos.nano.s.release.all;
-  ckb-cli = (import ./nix/dep/ckb-cli {});
-  ckb = (import ./nix/dep/ckb {});
+  ckb-cli = import ./nix/dep/ckb-cli {};
+  ckb = import ./nix/dep/ckb {};
 }
