ebf418668282934f35cda43a964c8128debac151
Jonathan D.K. Gibbons <jonored@gmail.com>
2020-03-10 13:09:41 -0400

    Re-enable debug test build (for prompts checking).

--- release.nix
+++ release.nix
@@ -3,7 +3,7 @@ let
 in {
   analysis-nanos = ledger-app-nervos.clangAnalysis.s.wallet;
   release-nanos = ledger-app-nervos.nano.s.release.all;
-  #debug-build = (import ./. {debug=1;}).nano.s.release.all;
+  debug-build = (import ./. {debug=1;}).nano.s.release.all;
   ckb-cli = import ./nix/dep/ckb-cli {};
   ckb = import ./nix/dep/ckb {};
 }
