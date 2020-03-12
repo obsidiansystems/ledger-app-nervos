1c132c51434161a1e8f02c27c06d561646e6114c
John Ericson <John.Ericson@Obsidian.Systems>
2020-03-12 15:43:21 +0000

    Get rust static lib building

--- default.nix
+++ default.nix
@@ -270,9 +270,15 @@ in rec {
     #nativeBuildInputs = [ pkgs.pkgconfig ];
     buildInputs = [ rustPackages.rust-std ];
     verifyCargoDeps = true;
+    target = "thumbv6m-none-eabi";
 
     # Cargo hash must be updated when Cargo.lock file changes.
-    cargoSha256 = "1ccxgp1f8dlvh7v4hmv9709y7rl16z7jjdgd8blzi09jqnlcysxi";
+    cargoSha256 = "1kdg77ijbq0y1cwrivsrnb9mm4y5vlj7hxn39fq1dqlrppr6fdrr";
+
+    # It is more reliable to trick a stable rustc into doing unstable features
+    # than use an unstable nightly rustc. Just because we want unstable
+    # langauge features doesn't mean we want a less tested implementation!
+    RUSTC_BOOTSTRAP = 1;
 
     meta = {
       platforms = pkgs.lib.platforms.all;
