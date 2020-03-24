fa43a17e8cfc863d268862a88a09c7c2570b34cb
Matthew Bauer <mjbauer95@gmail.com>
2020-03-24 14:23:27 -0400

    Don’t use nix run in test.sh
    This will fail in a nix builder and we can just pull in usbtool
    directly.

--- test.sh
+++ test.sh
@@ -34,7 +34,7 @@ if [ -n "$LEDGER_PROXY_PORT" ] ; then
   speculos --display headless bin/app.elf --button-port 5667 --deterministic-rng 42 |& $speculos_output_cmd $speculos_output_file &
   trap killSpeculos EXIT
 else
-  nix run -f ./nix/dep/usbtool.nix -c usbtool -v 0x2c97 log
+  usbtool -v 0x2c97 log
 fi
 
 appPid=$!
