5bd87cda50d661cf828a7b34fe3bbbc5aaf972e2
Divam <dfordivam@gmail.com>
2020-07-06 19:46:40 +0900

    Refactor: remove duplicate public_key from extended_key_pair_t

--- src/types.h
+++ src/types.h
@@ -47,8 +47,8 @@ typedef struct {
 } extended_public_key_t;
 
 typedef struct {
-    extended_public_key_t extended_public_key;
     key_pair_t key_pair;
+    uint8_t chain_code[CHAIN_CODE_DATA_SIZE];
 } extended_key_pair_t;
 
 #define MAX_BIP32_PATH 10
