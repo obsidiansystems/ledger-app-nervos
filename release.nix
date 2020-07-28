3ac96aa96a56d9365bdda8406f1d221aacbd7443
Jonathan D.K. Gibbons <jonored@gmail.com>
2020-07-28 13:30:40 -0400

    DO NOT MERGE: Only build the nano X app, to test CI.

--- release.nix
+++ release.nix
@@ -1,11 +1,5 @@
 let
   ledger-app-nervos = import ./. {};
 in {
-  analysis-nanos = ledger-app-nervos.clangAnalysis.s.wallet;
-  release-nanos = ledger-app-nervos.nano.s.release.all;
   release-nanox = ledger-app-nervos.nano.x.release.all;
-  debug-build = (import ./. {debug=true;}).nano.s.release.all;
-  ckb-cli = import ./nix/dep/ckb-cli {};
-  ckb = import ./nix/dep/ckb {};
-  usbtool = import ./nix/dep/usbtool.nix {};
 }
