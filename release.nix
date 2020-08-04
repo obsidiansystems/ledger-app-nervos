cd270fbc44e545a94b8082040eae1dbff3674c6c
Jonathan D.K. Gibbons <jonored@gmail.com>
2020-08-03 23:44:18 -0400

    Update to new thunks, use them properly.
    This fixes and reenables nano X builds in CI.

--- release.nix
+++ release.nix
@@ -3,7 +3,7 @@ let
 in {
   analysis-nanos = ledger-app-nervos.clangAnalysis.s.wallet;
   release-nanos = ledger-app-nervos.nano.s.release.all;
-  # release-nanox = ledger-app-nervos.nano.x.release.all; # Disabled until we figure out CI private repo issue
+  release-nanox = ledger-app-nervos.nano.x.release.all;
   debug-build = (import ./. {debug=true;}).nano.s.release.all;
   ckb-cli = import ./nix/dep/ckb-cli {};
   ckb = import ./nix/dep/ckb {};
