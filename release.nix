aeffaadaf8cb9b9f3515cb42dace4f3f9f7239d9
Elliot Cameron <eacameron@gmail.com>
2020-08-25 13:58:33 -0400

    Allow gitDescribe to be set in release.nix

--- release.nix
+++ release.nix
@@ -1,9 +1,9 @@
-{ runTest ? true }:
+{ runTest ? true, gitDescribe ? "TEST-dirty" }:
 let
-  ledger-app = import ./. { inherit runTest; };
+  ledger-app = import ./. { inherit runTest gitDescribe; };
 in {
   analysis-nanos = ledger-app.clangAnalysis.s.wallet;
   release-nanos = ledger-app.nano.s.release.all;
-  debug-build = (import ./. { debug = true; inherit runTest; }).nano.s.release.all;
+  debug-build = (import ./. { debug = true; inherit runTest gitDescribe; }).nano.s.release.all;
   usbtool = import ./nix/dep/usbtool.nix {};
 }
